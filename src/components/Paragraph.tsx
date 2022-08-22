92e98a3e3bbc1a84f911608d92a1b320b17c1be8
Max Davish <mdavish@yext.com>
2022-08-22 18:38:57 -0400

    removing paragraph icon and ability to select individual words

--- src/components/Paragraph.tsx
+++ src/components/Paragraph.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import ReactMarkdown from "react-markdown";
-import { BsParagraph } from "react-icons/bs";
 import { MdCancel } from "react-icons/md";
 import cx from "classnames";
 import { useStoreActions } from "./../store";
@@ -24,50 +23,50 @@ const Paragraph: React.FC<ParagraphProps> = ({
 }) => {
   const [hovering, isHovering] = useState(false);
   const clearSelectedParagraphs = useStoreActions(actions => actions.clearSelectedParagraphs);
-  const handleWordSelection = useStoreActions(actions => actions.handleWordSelection);
   return (
     <div className="flex flex-row">
       <div className="grid group">
         <button
           onMouseEnter={() => isHovering(true)}
           onMouseLeave={() => isHovering(false)}
-          onClick={() => onSelect(index)}
           className="mx-auto my-auto"
-        >
-          <BsParagraph className={cx(
-            "my-auto mx-auto mr-2 transition-colors",
-            selected ? "text-blue-800" : "text-gray-500 hover:text-blue-700",
-          )} />
-        </button>
+        />
       </div>
-      <div
-        onMouseUp={() => {
-          const selection = document.getSelection();
-          if (selection) {
-            handleWordSelection(selection);
-          }
-        }}
-        className={cx(
-          "relative prose-xs transition-all delay-100 p-1 w-full",
-          (selected && isFirstSelection) && "rounded-t-md",
-          (selected && isLastSelection) && "rounded-b-md",
-          (hovering && !selected) && "bg-blue-50  ring-1 ring-blue-300 rounded-md",
-          selected && "bg-blue-200",
-        )}
-      >
+      <div className="w-full relative">
+        <button
+          onClick={() => {
+            console.log("Clicking the regular button")
+            onSelect(index)
+          }}
+          onMouseEnter={() => isHovering(true)}
+          onMouseLeave={() => isHovering(false)}
+          className={cx(
+            "text-left w-full",
+            "relative prose-xs transition-all delay-100 p-1 w-full",
+            (selected && isFirstSelection) && "rounded-t-md",
+            (selected && isLastSelection) && "rounded-b-md",
+            (hovering && !selected) && "bg-blue-50 ring-1 ring-blue-300 rounded-md",
+            selected && "bg-blue-200",
+          )}
+        >
+          <ReactMarkdown
+            linkTarget={"__blank"}
+          >
+            {children}
+          </ReactMarkdown>
+        </button>
         {
           (selected && isFirstSelection) &&
           <button
-            onClick={() => clearSelectedParagraphs()}
-            className="grid -right-2 -top-2 z-10 absolute rounded-full bg-white" >
+            onClick={(event) => {
+              console.log("Clicking the X button")
+              event.preventDefault();
+              clearSelectedParagraphs()
+            }}
+            className="grid -right-2 -top-2 z-50 absolute rounded-full bg-white" >
             <MdCancel className="text-xl my-auto mx-auto text-gray-700 hover:text-gray-900" />
           </button>
         }
-        <ReactMarkdown
-          linkTarget={"__blank"}
-        >
-          {children}
-        </ReactMarkdown>
       </div>
     </div >
   )
